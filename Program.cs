7f65b6115856dcec95b967a348e7c6938cbea2b0
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-08-21 21:16:07 -0600

    Set up main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Exercism_Exercises
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+
+        }
+
+    }
+}
