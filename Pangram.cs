2c79924f375dba16cbcbab92be6192cc81f91851
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-08-26 19:38:33 -0600

    The Panagram exercise was completed, with 4 iterations

---
+++ Pangram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panagram
+{
+    public static class Pangram
+    {
+        public static bool IsPangram1(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return false;
+
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            input = input.ToLower();
+
+            return letters.All(i => input.Contains(i));
+        }
+        public static bool IsPangram2(string input)
+        {
+            HashSet<char> matches = new();
+            foreach (char c in input.ToLower())
+            {
+                if (Char.IsLetter(c))
+                    matches.Add(c);
+            }
+
+            return matches.Count == 26;
+        }
+        public static bool IsPangram3(string input)
+        {
+            HashSet<char> matches = new(input.ToLowerInvariant().Where(c => Char.IsLetter(c)));
+            return matches.Count == 26;
+        }
+        public static bool IsPangram(string input) => input.ToLowerInvariant().Where(c => Char.IsLetter(c)).Distinct().Count() == 26;
+
+        
+    }
+}
