29cddae9372a379a8c55fbcdad0532fc8c558370
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-08-30 16:59:02 -0600

    Completed the Rotational Cipher exercise

---
+++ Exercises/RotationalCipher.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace RotationalCipher
+{
+    public static class RotationalCipher
+    {
+        private static readonly char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        public static string Rotate(string text, int shiftKey)
+        {
+            if (string.IsNullOrEmpty(text))
+                return "";
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    if (char.IsUpper(text[i]))
+                    {
+                        int shiftValue = ((int)text[i] + shiftKey - 65) % 26;
+                        result.Append(char.ToUpper(alphabet[shiftValue]));
+                    }
+                    else
+                    {
+                        int shiftValue = ((int)text[i] + shiftKey - 97) % 26;
+                        result.Append(alphabet[shiftValue]);
+                    }
+                }
+                else
+                {
+                    result.Append(text[i]);
+                }
+            }
+            return result.ToString();
+        }
+    }
+}
