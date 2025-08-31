bae60562ee84dc0c85ee27465b9fc1c0fc669bca
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-08-31 16:37:19 -0600

    TThe Robot Name exercise was completed

---
+++ Exercises/RobotName.cs
@@ -0,0 +1,49 @@
+﻿namespace Exercism_Exercises.Exercises
+{
+
+    /* Instructions
+        Manage robot factory settings.
+        When a robot comes off the factory floor, it has no name.
+        The first time you turn on a robot, a random name is generated in the format of two uppercase letters followed by three digits, such as RX837 or BC811.
+        Every once in a while we need to reset a robot to its factory settings, which means that its name gets wiped. The next time you ask, that robot will respond with a new random name.
+        The names must be random: they should not follow a predictable sequence. Using random names means a risk of collisions. Your solution must ensure that every existing robot has a unique name. 
+     */
+
+    public class Robot
+    {
+        private static readonly Random _random = new();
+        private static readonly HashSet<string> _robotNamesList = [];
+
+        protected string name = GenerateRobotName();
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public void Reset()
+        {
+            _robotNamesList.Remove(Name);
+            name = GenerateRobotName();
+        }
+
+        private static string GenerateRobotName()
+        {
+            if (_robotNamesList.Count == 676000)//670000 (26*26*10*10*10) are all the posible combinations for the robot names.
+                throw new Exception("There are no more robot names that can generate.");
+
+            string result = string.Concat(GenerateLetter(), GenerateLetter(), GenerateNumber(), GenerateNumber(), GenerateNumber());
+
+            if (_robotNamesList.Add(result))
+                return result;
+            else
+                return GenerateRobotName();
+        }
+
+        private static char GenerateLetter() => (char)_random.Next(65, 91);
+        private static int GenerateNumber() => _random.Next(0, 10);
+    }
+}
