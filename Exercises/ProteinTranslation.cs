4f535de3468d784dd9c3d642960a8cdc9edfbc30
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-08-31 18:00:08 -0600

    Completed the Protein Translation exercise

---
+++ Exercises/ProteinTranslation.cs
@@ -0,0 +1,53 @@
+﻿namespace Exercism_Exercises.Exercises
+{
+    public static class ProteinTranslation
+    {
+        public static string[] Proteins(string strand)
+        {
+            List<string> result = [];
+
+            for (int i = 0; i < strand.Length; i+=3)
+            {
+                string protein = strand.Substring(i,3);
+                switch (protein)
+                {
+                    case "AUG":
+                        result.Add("Methionine");
+                        break;
+
+                    case "UUU" or "UUC":
+                        result.Add("Phenylalanine");
+                        break;
+
+                    case "UUA" or "UUG":
+                        result.Add("Leucine");
+                        break;
+
+                    case "UCU" or "UCC" or "UCA" or "UCG":
+                        result.Add("Serine");
+                        break;
+
+                    case "UAC" or "UAU":
+                        result.Add("Tyrosine");
+                        break;
+
+                    case "UGC" or "UGU":
+                        result.Add("Cysteine");
+                        break;
+
+                    case "UGG":
+                        result.Add("Tryptophan");
+                        break;
+
+                    case "UAA" or "UAG" or "UGA":
+                        return result.ToArray();
+
+                    default:
+                        throw new Exception($"{protein} doesn't exist.");
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
