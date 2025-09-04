3fb7c2cf2cfa3ac6f99a79e4cacc824c71b57dea
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-09-03 20:29:46 -0600

    Refactor CollatzConjecture to use recursion
    The `Steps` method in `CollatzConjecture.cs` has been refactored from an iterative approach to a recursive one.

--- Exercises/CollatzConjecture.cs
+++ Exercises/CollatzConjecture.cs
@@ -6,16 +6,19 @@
         {
             if (number <= 0)
                 throw new ArgumentOutOfRangeException($"The {number} has to be greater than 0.");
-            else
-            {
-                int steps = 0;
-                while (number != 1)
-                {
-                    number = (number % 2 == 0) ? number / 2 : (number * 3) + 1;
-                    steps++;
-                }
-                return steps;
-            }
+
+            return Steps(number, stepCount: 0); 
+        }
+
+        private static int Steps(int number, int stepCount)
+        {
+            if (number == 1)
+                return stepCount;
+
+            if (number % 2 == 0)
+                return Steps(number / 2, stepCount + 1);
+
+            return Steps((number * 3) + 1, stepCount + 1);
         }
     }
 }
