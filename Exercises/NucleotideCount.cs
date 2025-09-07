6b7e6694dbd6c3666bdcb66238bc7c8f5f852017
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-09-06 18:49:47 -0600

    completed the Nucleotide Count exercise

---
+++ Exercises/NucleotideCount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism_Exercises.Exercises
+{
+    public static class NucleotideCount
+    {
+        public static IDictionary<char, int> Count(string sequence)
+        {
+            Dictionary<char, int> nucleotideCounts = new()
+            {
+                { 'A', 0 }, { 'C', 0 }, { 'G', 0 }, { 'T', 0 },
+            };
+
+            if (string.IsNullOrWhiteSpace(sequence))
+                return nucleotideCounts;
+
+            foreach (char nucleotide in sequence)
+            {
+                if (nucleotideCounts.TryGetValue(nucleotide, out int count))
+                    nucleotideCounts[nucleotide]++;
+                else
+                    throw new ArgumentException($"{nucleotide} is not a valid nucleotide.");
+            }
+
+            return nucleotideCounts;
+        }
+
+    }
+}
