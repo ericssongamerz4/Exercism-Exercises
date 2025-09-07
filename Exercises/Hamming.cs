75907358c8a048d0e4caeedb60bf3434615de7be
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-09-06 19:49:37 -0600

    Completed the Hamming exercise

---
+++ Exercises/Hamming.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism_Exercises.Exercises
+{
+    public static class Hamming
+    {
+        public static int Distance(string firstStrand, string secondStrand)
+        {
+            if (firstStrand.Length != secondStrand.Length)
+                throw new ArgumentException("Strands must have the same length.");
+
+            int result = 0;
+
+            if(firstStrand == secondStrand)
+                return result;
+
+
+            for (int i = 0; i < firstStrand.Length; i++)
+            {
+                if (firstStrand[i] != secondStrand[i])
+                    result++;
+            }
+
+            return result;
+        }
+    }
+}
