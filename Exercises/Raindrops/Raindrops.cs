dc2c71ab5fe0eed7e2c16844645a130e6cb188fd
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-09-08 17:14:39 -0600

    Completed the Raindrops exercise using if statements

---
+++ Exercises/Raindrops/Raindrops.cs
@@ -0,0 +1,24 @@
+namespace Exercism_Exercises.Exercises.Raindrops
+{
+    public static class Raindrops
+    {
+        public static string Convert(int number)
+        {
+            string result = string.Empty;
+
+            if (number % 3 == 0)
+                result += "Pling";
+
+            if (number % 5 == 0)
+                result += "Plang";
+
+            if (number % 7 == 0)
+                result += "Plong";
+
+            return string.IsNullOrEmpty(result) ? number.ToString() : result;
+        }
+    }
+
+}//Made by ericssongamerz4
+
+                
