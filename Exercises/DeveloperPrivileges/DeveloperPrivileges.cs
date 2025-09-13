ab9a9d1cba7ec29e2df9d13d57391c61a49d2fcf
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-09-13 14:35:01 -0600

    Completed the Developer Privileges exercise

---
+++ Exercises/DeveloperPrivileges/DeveloperPrivileges.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Exercism_Exercises.Exercises.DeveloperPrivileges
+{
+    public class Authenticator
+    {
+        private static Identity GetNewIdentity(string email, string eyeColor, decimal philtrumWidth, List<string> nameAndAddress) => new Identity
+        {
+            Email = email,
+            FacialFeatures = new FacialFeatures { EyeColor = eyeColor, PhiltrumWidth = philtrumWidth },
+            NameAndAddress = nameAndAddress
+        };
+
+
+        public Identity Admin => GetNewIdentity("admin@ex.ism", "green", 0.9M, ["Chanakya", "Mumbai", "India"]);
+
+        public IDictionary<string, Identity> Developers => new Dictionary<string, Identity>
+        {
+            { "Bertrand", GetNewIdentity("bert@ex.ism", "blue",  0.8M, ["Bertrand", "Paris", "France"]) },
+            { "Anders", GetNewIdentity("anders@ex.ism", "brown",  0.85M, ["Anders", "Redmond", "USA"])},
+        };
+    }
+
+    //**** please do not modify the FacialFeatures class ****
+    public class FacialFeatures
+    {
+        public required string EyeColor { get; set; }
+        public required decimal PhiltrumWidth { get; set; }
+    }
+
+    //**** please do not modify the Identity class ****
+    public class Identity
+    {
+        public required string Email { get; set; }
+        public required FacialFeatures FacialFeatures { get; set; }
+        public required IList<string> NameAndAddress { get; set; }
+    }
+
+}//Made by ericssonGamerz4
+
