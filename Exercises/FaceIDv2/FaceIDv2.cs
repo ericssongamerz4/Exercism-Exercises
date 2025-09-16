62b4f9978699f950b1eda4ea4c6a198f73b2a917
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-09-16 17:54:26 -0600

    Completed the Face ID 2.0 exercise.

---
+++ Exercises/FaceIDv2/FaceIDv2.cs
@@ -0,0 +1,41 @@
+namespace Exercism_Exercises.Exercises.FaceIDv2
+{
+    public class FacialFeatures
+    {
+        public string EyeColor { get; }
+        public decimal PhiltrumWidth { get; }
+        public FacialFeatures(string eyeColor, decimal philtrumWidth)
+        {
+            EyeColor = eyeColor;
+            PhiltrumWidth = philtrumWidth;
+        }
+        public override bool Equals(object? obj) => obj is FacialFeatures face && EyeColor == face.EyeColor && PhiltrumWidth == face.PhiltrumWidth;
+        public override int GetHashCode() => HashCode.Combine(EyeColor, PhiltrumWidth);
+    }
+
+    public class Identity
+    {
+        public string Email { get; }
+        public FacialFeatures FacialFeatures { get; }
+        public Identity(string email, FacialFeatures facialFeatures)
+        {
+            Email = email;
+            FacialFeatures = facialFeatures;
+        }
+        public override bool Equals(object? obj) => obj is Identity other && Email == other.Email && FacialFeatures.Equals(other.FacialFeatures);
+        public override int GetHashCode() => HashCode.Combine(Email, FacialFeatures.GetHashCode());
+    }
+
+    public class Authenticator
+    {
+        private static readonly Identity _admin = new Identity("admin@exerc.ism", new FacialFeatures("green", 0.9m));
+        private readonly HashSet<Identity> _identitySet = new();
+
+        public static bool AreSameFace(FacialFeatures faceA, FacialFeatures faceB) => faceA.Equals(faceB);
+        public bool IsAdmin(Identity identity) => _admin.Equals(identity);
+        public bool Register(Identity identity) => _identitySet.Add(identity);
+        public bool IsRegistered(Identity identity) => _identitySet.Contains(identity);
+        public static bool AreSameObject(Identity identityA, Identity identityB) => ReferenceEquals(identityA, identityB);
+    }
+}
+//Made by ericssonGamerz4
