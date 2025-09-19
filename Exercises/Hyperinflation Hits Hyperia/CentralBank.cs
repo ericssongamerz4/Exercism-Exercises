2e60b1abb923a2e9fd35b56db7d3dabc5eddb84d
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-09-18 18:15:59 -0600

    Completed the Hyperinflation Hits Hyperia exercise.

---
+++ Exercises/Hyperinflation Hits Hyperia/CentralBank.cs
@@ -0,0 +1,33 @@
+public static class CentralBank
+{
+    public static string DisplayDenomination(long @base, long multiplier)
+    {
+        try
+        {
+            return $"{checked(@base * multiplier)}";
+        }
+        catch (OverflowException)
+        {
+            return "*** Too Big ***";
+        }
+    }
+    public static string DisplayGDP(float @base, float multiplier)
+    {
+        float result = @base * multiplier;
+        if (float.IsInfinity(result))
+            return "*** Too Big ***";
+
+        return $"{result}";
+    }
+    public static string DisplayChiefEconomistSalary(decimal salaryBase, decimal multiplier)
+    {
+        try
+        {
+            return $"{salaryBase * multiplier}";
+        }
+        catch (OverflowException)
+        {
+            return "*** Much Too Big ***";
+        }
+    }
+}//Made by ericssonGamerz4
