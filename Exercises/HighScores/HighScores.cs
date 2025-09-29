1950fed9e5167979bbdc3b86595e747c08b868f1
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-09-28 21:55:14 -0600

    Completed the High Scores exercise.
    Add HighScores class and corresponding tests
    
    Introduce the `HighScores` class for managing score lists,
    including methods for retrieving scores, latest score,
    personal best, and top three scores. Implement tests in
    `HighScoresTests` to validate functionality using Xunit.

---
+++ Exercises/HighScores/HighScores.cs
@@ -0,0 +1,21 @@
+namespace Exercism_Exercises.Exercises.HighScores
+{
+    public class HighScores
+    {
+        private readonly List<int> scoreList;
+        public HighScores(List<int> list)
+        {
+            scoreList = list;
+        }
+
+        public List<int> Scores() => scoreList;
+
+        public int Latest() => scoreList.Last();
+
+        public int PersonalBest() => scoreList.Max();
+
+        public List<int> PersonalTopThree() => scoreList.OrderByDescending(x => x).Take(3).ToList();
+    }
+}//Made by ericssonGamerz4
+
+                
