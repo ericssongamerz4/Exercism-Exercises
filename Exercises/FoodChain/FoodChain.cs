905b7085a43a524132bfbc22dacf979cd0d9c846
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-09-30 21:43:14 -0600

    Refactor FoodChain class for improved maintainability
    Replaced hardcoded strings with dictionaries for verses, enhancing readability. Overloaded the `Recite` method to support a range of verses and introduced helper methods for dynamic line generation. Streamlined the structure by removing the static list of verses and utilizing LINQ for verse generation.

--- Exercises/FoodChain/FoodChain.cs
+++ Exercises/FoodChain/FoodChain.cs
@@ -2,63 +2,54 @@ namespace Exercism_Exercises.Exercises.FoodChain
 {
     public static class FoodChain
     {
-        private static readonly string First = "I know an old lady who swallowed a fly.\n";
-        private static readonly string FirstLastVerse = "I don't know why she swallowed the fly. Perhaps she'll die.";
+        private static readonly string _verseBreak = "\n\n";
 
-        private static readonly string Second = "I know an old lady who swallowed a spider.\n" +
-                                                "It wriggled and jiggled and tickled inside her.\n";
-        private static readonly string SecondLastVerse = "She swallowed the spider to catch the fly.\n";
-
-        private static readonly string Third = "I know an old lady who swallowed a bird.\n" +
-                                               "How absurd to swallow a bird!\n";
-        private static readonly string ThirdLastVerse = "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n";
-
-        private static readonly string Forth = "I know an old lady who swallowed a cat.\n" +
-                                               "Imagine that, to swallow a cat!\n";
-        private static readonly string ForthLastVerse = "She swallowed the cat to catch the bird.\n";
-
-        private static readonly string Fifth = "I know an old lady who swallowed a dog.\n" +
-                                               "What a hog, to swallow a dog!\n";
-        private static readonly string FifthLastVerse = "She swallowed the dog to catch the cat.\n";
-
-        private static readonly string Sixth = "I know an old lady who swallowed a goat.\n" +
-                                               "Just opened her throat and swallowed a goat!\n";
-        private static readonly string SixthLastVerse = "She swallowed the goat to catch the dog.\n";
-
-        private static readonly string Seventh = "I know an old lady who swallowed a cow.\n" +
-                                                  "I don't know how she swallowed a cow!\n";
-        private static readonly string SeventhLastVerse = "She swallowed the cow to catch the goat.\n";
-
-        private static readonly string Last = "I know an old lady who swallowed a horse.\n" +
-                                              "She's dead, of course!";
-
-        private static readonly List<string> Song = new List<string>()
+        private static readonly Dictionary<int, string> _animals = new()
         {
-            "",
-            First + FirstLastVerse,
-            Second + SecondLastVerse + FirstLastVerse,
-            Third + ThirdLastVerse + SecondLastVerse + FirstLastVerse,
-            Forth + ForthLastVerse + ThirdLastVerse + SecondLastVerse + FirstLastVerse,
-            Fifth + FifthLastVerse + ForthLastVerse + ThirdLastVerse + SecondLastVerse + FirstLastVerse,
-            Sixth + SixthLastVerse + FifthLastVerse + ForthLastVerse + ThirdLastVerse + SecondLastVerse + FirstLastVerse,
-            Seventh + SeventhLastVerse + SixthLastVerse + FifthLastVerse + ForthLastVerse + ThirdLastVerse + SecondLastVerse + FirstLastVerse,
-            Last,
+            {1,"fly" },
+            {2,"spider" },
+            {3,"bird" },
+            {4,"cat" },
+            {5,"dog" },
+            {6,"goat" },
+            {7,"cow" },
+            {8,"horse" },
         };
-
-        public static string Recite(int verseNumber) => Song[verseNumber];
-
-        public static string Recite(int startVerse, int endVerse)
+        private static readonly Dictionary<int, string> _secondVerse = new()
         {
-            string result = "";
+            {1, string.Empty },
+            {2,"It wriggled and jiggled and tickled inside her.\n" },
+            {3,"How absurd to swallow a bird!\n" },
+            {4,"Imagine that, to swallow a cat!\n" },
+            {5,"What a hog, to swallow a dog!\n" },
+            {6,"Just opened her throat and swallowed a goat!\n" },
+            {7,"I don't know how she swallowed a cow!\n" },
+            {8,"She's dead, of course!" },
+        };
+        public static string Recite(int verseNumber) => string.Concat(FirstLine(verseNumber), SecondLine(verseNumber), MiddleLines(verseNumber), LastLine(verseNumber));
 
-            foreach (string verse in Song.GetRange(startVerse, endVerse))
-            {
-                result += verse;
-                if (verse != Recite(endVerse))
-                    result += "\n\n";
-            }
-            return result;
-        }
+        public static string Recite(int startVerse, int endVerse) =>
+        String.Join(_verseBreak,
+            Enumerable.Range(startVerse, endVerse)
+            .Select(currentVerse => Recite(currentVerse))
+        );
+
+        private static string FirstLine(int verseNumber) => $"I know an old lady who swallowed a {_animals[verseNumber]}.\n";
+
+        private static string SecondLine(int verseNumber) => _secondVerse[verseNumber];
+
+        private static string MiddleLine(int verseNumber) => $"She swallowed the {_animals[verseNumber]} to catch the {_animals[verseNumber - 1]}{SpiderBonus(verseNumber)}.\n";
+
+        private static string MiddleLines(int verseNumber) => (verseNumber == 8) ? string.Empty :
+        String.Concat(
+            Enumerable.Range(2, verseNumber - 1)
+            .Reverse()
+            .Select(currentVerse => MiddleLine(currentVerse))
+        );
+
+        private static string SpiderBonus(int verseNumber) => (verseNumber != 3) ? string.Empty : " that wriggled and jiggled and tickled inside her";
+
+        private static string LastLine(int verseNumber) => (verseNumber == 8) ? string.Empty : $"I don't know why she swallowed the fly. Perhaps she'll die.";
     }
 }//Made by ericssonGamerz4
 
