37bc03fb457684c2134af2457b53e39e5ba80c20
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-11-06 20:45:49 -0600

    Completed the Eliuds Eggs exercise.

---
+++ Exercises/EliudsEggs/EliudsEggs.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Exercism_Exercises.Exercises.EliudsEggs;
+
+public static class EliudsEggs
+{
+    public static int EggCount(int encodedCount)
+    {
+        int count = 0;
+        while (encodedCount > 0) {
+
+            count += encodedCount & 1;
+            encodedCount >>= 1;
+        }
+        return count;
+    }
+}
+
+//Made by ericssonGamerz4
