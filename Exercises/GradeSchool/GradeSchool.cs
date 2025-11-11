609722242d13f163a6b1f0149e803e9de9f61b20
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-11-11 15:19:19 -0600

    Completed the Grade School exercise.

---
+++ Exercises/GradeSchool/GradeSchool.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Exercism_Exercises.Exercises.GradeSchool;
+/*
+Instructions
+Given students' names along with the grade they are in, create a roster for the school.
+
+In the end, you should be able to:
+
+Add a student's name to the roster for a grade:
+"Add Jim to grade 2."
+"OK."
+Get a list of all students enrolled in a grade:
+"Which students are in grade 2?"
+"We've only got Jim right now."
+Get a sorted list of all students in all grades. Grades should be sorted as 1, 2, 3, etc., 
+and students within a grade should be sorted alphabetically by name.
+"Who is enrolled in school right now?"
+"Let me think. We have Anna, Barb, and Charlie in grade 1, Alex, Peter, and Zoe in grade 2, and Jim in grade 5. 
+So the answer is: Anna, Barb, Charlie, Alex, Peter, Zoe, and Jim."
+Note that all our students only have one name (it's a small town, what do you want?), 
+and each student cannot be added more than once to a grade or the roster. 
+If a test attempts to add the same student more than once, your implementation should indicate that this is incorrect. 
+ 
+ */
+
+public class GradeSchool
+{
+
+    private readonly Dictionary<string, int> studentGrades = [];
+
+    public bool Add(string student, int grade) => studentGrades.TryAdd(student, grade);
+
+    public IEnumerable<string> Roster()
+    {
+        return studentGrades.OrderBy(x => x.Value)
+                            .ThenBy(x => x.Key)
+                            .Select(x => x.Key);
+    }
+
+    public IEnumerable<string> Grade(int grade)
+    {
+        return studentGrades.Where(x => x.Value == grade)
+                            .OrderBy(x => x.Value)
+                            .ThenBy(x => x.Key)
+                            .Select(x => x.Key);
+    }
+}
+//Made by ericssonGamerz4
