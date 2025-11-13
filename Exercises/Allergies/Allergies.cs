d46357c922f8c51bd77a763572a4def4c2e096e0
Erick Montaño <149527501+ericssongamerz4@users.noreply.github.com>
2025-11-12 19:38:55 -0600

    Completed de Allergies exercise. using flag enums

---
+++ Exercises/Allergies/Allergies.cs
@@ -0,0 +1,33 @@
+namespace Exercism_Exercises.Exercises.Allergies;
+
+[Flags]
+public enum Allergen
+{
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128
+}
+
+public class Allergies
+{
+    private readonly Allergen UserAllergies;
+
+    public Allergies(int mask)
+    {
+        UserAllergies = (Allergen) mask;      
+    }
+
+    public bool IsAllergicTo(Allergen allergen) => UserAllergies.HasFlag(allergen);
+
+    public Allergen[] List() => Enum.GetValues(typeof(Allergen))
+                                    .Cast<Allergen>()
+                                    .Where(allergen => IsAllergicTo(allergen))
+                                    .ToArray();
+}
+//Made by ericssonGamerz4
+
